f4d7d5aa1011ea89314b368dce0abb01a6af6788
Bryce Kellogg <bryce@kellogg.org>
2021-03-26 11:24:36 -0700

    Cloud: use env vars to deploy

--- cloud/aws-deploy.sh
+++ cloud/aws-deploy.sh
@@ -7,7 +7,9 @@ zip lambda.zip lambda.py
 aws s3 cp cloud.yaml s3://tide-project
 aws s3 cp lambda.zip s3://tide-project/$LAMBDA_SOURCE
 
-aws cloudformation update-stack --stack-name tide-gauge \
+aws cloudformation create-stack --stack-name tide-gauge \
                                 --template-body file://cloud.yaml \
                                 --capabilities CAPABILITY_IAM \
-                                --parameters ParameterKey=LambdaSource,ParameterValue=$LAMBDA_SOURCE
+                                --parameters ParameterKey=LambdaSource,ParameterValue=$LAMBDA_SOURCE \
+                                             ParameterKey=DeviceID,ParameterValue=${PARTICLE_DEVICE} \
+                                             ParameterKey=ParticleToken,ParameterValue=${PARTICLE_TOKEN}
