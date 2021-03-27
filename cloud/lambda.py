343c9b1a6ef86cb6dd1a3c8cabaed99229ba2041
Bryce Kellogg <bryce@kellogg.org>
2021-03-26 18:21:46 -0700

    Cloud: lambda can now read from database

--- cloud/lambda.py
+++ cloud/lambda.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from schema import *
+from datetime import datetime, timezone
 
 from pprint import pprint
 
@@ -119,12 +120,101 @@ def saveDeviceData(data, databaseName='MudFlatsTideData', tableName='DeviceData'
     return {'statusCode': 200, 'body': 'OK'}
 
 
-def readSensorData():
-    pass
+def readSensorData(databaseName='MudFlatsTideData', tableName='SensorData'):
+    """
+
+    """
+
+    # Build query string
+    queryString = f"""
+        SELECT * FROM "{databaseName}"."{tableName}"
+    """
+    # TODO: add filtering (WHERE clause)
+
+    # Perform query
+    client = boto3.client('timestream-query')
+    response = client.query(QueryString=queryString)
+
+    # Parse response
+    columnInfo = response['ColumnInfo']
+    rows = response['Rows']
+
+    # Get indices of each interesting known column
+    indexID = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'id')
+    indexMeas = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'measure_value::bigint')
+    indexName = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'measure_name')
+    indexTime = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'time')
+
+    # Build wanted response format
+    records = []
+    for r in rows:
+        deviceID = r['Data'][indexID]['ScalarValue']
+        data = r['Data'][indexMeas]['ScalarValue']
+        time = r['Data'][indexTime]['ScalarValue']
+        time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.000000000')
+        time = time.replace(tzinfo=timezone.utc).timestamp()
+
+        record = {'id': deviceID, 'time': time, 'data': data}
+        records.append(record)
+
+    # Return data
+    return {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
 
 
-def readDeviceData():
-    pass
+def readDeviceData(databaseName='MudFlatsTideData', tableName='DeviceData'):
+    """
+         [
+             {"id": <str>, "time": <int>, <deviceMeasure>: <int>},
+             {"id": <str>, "time": <int>, <deviceMeasure>: <int>},
+             ...
+          ]
+
+          Really need to be able to filter by deviceMeasure name as well
+          as by timestamp
+
+    """
+
+    # Build query string
+    queryString = f"""
+        SELECT * FROM "{databaseName}"."{tableName}"
+    """
+    # TODO: add filtering (WHERE clause)
+
+    # Perform query
+    client = boto3.client('timestream-query')
+    response = client.query(QueryString=queryString)
+
+    # Parse response
+    columnInfo = response['ColumnInfo']
+    rows = response['Rows']
+
+    # Get indices of each interesting known column
+    indexID = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'id')
+    indexMeas = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'measure_value::double')
+    indexName = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'measure_name')
+    indexTime = next(i for i,_ in enumerate(columnInfo) if _['Name'] == 'time')
+
+    # Build wanted response format
+    records = []
+    for r in rows:
+        deviceID = r['Data'][indexID]['ScalarValue']
+        name = r['Data'][indexName]['ScalarValue']
+        data = r['Data'][indexMeas]['ScalarValue']
+        time = r['Data'][indexTime]['ScalarValue']
+        time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.000000000')
+        time = time.replace(tzinfo=timezone.utc).timestamp()
+
+        record = {'id': deviceID, 'time': time, name: data}
+        records.append(record)
+
+    # Return data
+    return {
+        'statusCode': 200,
+        'body': json.dumps(records)
+    }
 
 
 def test():
@@ -149,8 +239,9 @@ def test():
        """
 
     # res = saveSensorData(sampleSensorData)
-    # print(res)
-    res = saveDeviceData(sampleDeviceData)
+    # res = saveDeviceData(sampleDeviceData)
+    # res = readSensorData()
+    res = readDeviceData()
     print(res)
 
 
